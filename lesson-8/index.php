c70d54a31276c76703d94604dff82edfb16d5e8f
MarlonDemas <demasmarlon@gmail.com>
2018-11-16 11:09:37 +0200

    Added today's lesson on uploading files and moving them

---
+++ lesson-8/index.php
@@ -0,0 +1,24 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Gimme dat file</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+ </head>
+ <body>
+     <form action="upload.php" method="post" enctype="multipart/form-data">
+     
+     <input type="file" name="image1">
+     <input type="file" name="image2">
+     <input type="file" name="image3">
+     <input type="file" name="image4">
+     <input type="file" name="image5">
+     <input type="file" name="image6">
+     <input type="file" name="image7">
+     <input type="file" name="image8">
+     <input type="submit">
+
+     </form>
+ </body>
+ </html>
---
+++ lesson-8/upload.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <title>Display</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+ </head>
+ <body>
+     <!-- <?php
+        var_dump($_FILES);
+
+        $target_dir = "images/";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+
+        move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+     ?>
+
+     <img src="<?php echo $target_file?>" alt=""> -->
+
+     <?php
+        var_dump($_FILES);
+
+        $target_dir = "images/";
+
+        foreach($_FILES as $file){
+
+            $target_file = $target_dir . basename($file["name"]);
+            move_uploaded_file($file["tmp_name"], $target_file);
+            echo "<img src=\"$target_file\">";
+        }
+     ?>
+
+
+ </body>
+ </html>
